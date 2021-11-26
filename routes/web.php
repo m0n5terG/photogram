f92f85aafe03101513115a5f1235852b440e55e1
m0n5terG <mikotai@yahoo.com>
2021-11-26 21:24:26 +0800

    added route to profile

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/profile',
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
