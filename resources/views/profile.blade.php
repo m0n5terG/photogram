0de2b7abba0d1f409eed2755d8ba73da4a2a6e39
m0n5terG <mikotai@yahoo.com>
2021-11-27 11:03:19 +0800

    updated changes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,8 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-3">
            <img class="rounded-circle" width="150" height="150" src="/storage/{{ $profile->image }}">
-       </div>
-       <div class="col-md-9 pt-2">
+        </div>
+        <div class="col-md-9 pt-2">
            <h3>{{ $user->name }}</h3>
            <span><strong>{{ $numPosts }}</strong> posts</span>
            {{-- <div class="pt-3">{{$profile->description}}</div>
@@ -21,8 +21,17 @@
                     <div class="pt-2"><a href="/profile/edit">Edit profile</a></div>
 
                 @endif
-       </div>
-   </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        @foreach($posts as $post)
+            <div class="col-4 mb-5">
+                <a href="/post/{{$post->id}}">
+                    <img src="/storage/{{$post->image}}" class="w-100">
+                </a>
+            </div>
+        @endforeach
+    </div>
 </div>
 
 @endsection
